33ba2413a9d3d5a51ebeaac9e0a4729713eb7790
Alex <alejandrotorquemada@gmail.com>
2025-01-21 22:45:53 +0100

    Initial commit

---
+++ little-lemon-website/src/components/ConfirmedBooking.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ConfirmedBooking = () => {
+    return (
+        <div className="confirm">
+            <div>
+                <h1>Booking has been <spam>confirmed!</spam></h1>
+            </div>
+        </div>
+    );
+};
+
+export default ConfirmedBooking;
